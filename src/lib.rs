f9c864ed33fdebd408a372d15ca98181ada3251a
Artur Kowalski <arturkow2000@gmail.com>
2021-11-25 16:03:19 +0100

    Add smoltcp integration

--- src/lib.rs
+++ src/lib.rs
@@ -13,5 +13,9 @@ mod dummy_log;
 
 mod buffer;
 mod eem;
+#[cfg(feature = "smoltcp_integration")]
+mod smoltcp;
 
+#[cfg(feature = "smoltcp_integration")]
+pub use crate::smoltcp::*;
 pub use eem::EthernetDriver;
